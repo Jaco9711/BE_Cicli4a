6cb4b33972910e6114d0a2a10fd29f9c33f8f187
Jaco9711 <jaco9711@gmail.com>
2022-11-23 22:27:52 -0500

    first commit

---
+++ Controladores/MesaControlador.py
@@ -0,0 +1,27 @@
+from Repositorios.MesaRepositorio import MesaRepositorio
+from Modelos.Mesa import Mesa
+
+class MesaControlador():
+    
+    def __init__(self):
+        self.repositorioMesa = MesaRepositorio()
+    
+    def index(self):
+        return self.repositorioMesa.findAll()
+    
+    def create(self, infoMesa):
+        nuevaMesa = Mesa(infoMesa)
+        return self.repositorioMesa.save(nuevaMesa)
+    
+    def show(self, id):
+        laMesa = Mesa(self.repositorioMesa.findById(id))
+        return laMesa.__dict__
+    
+    def update(self, id, infoMesa):
+        MesaActual = Mesa(self.repositorioMesa.findById(id))
+        MesaActual.numero = infoMesa["numero"]
+        MesaActual.cantidad_inscritos = infoMesa["cantidad_inscritos"]
+        return self.repositorioMesa.save(MesaActual)
+    
+    def delete(self, id):
+        return self.repositorioMesa.delete(id)
